842b5c4924d45fb3f821a00f59ea43e61cd111ac
nathanmacfarlane <nathanmmacfarlane@gmail.com>
2018-08-22 00:04:43 -0700

    no swiftlint errors but exit code 1 on device only

--- Take/Custom Cells/RouteCell.swift
+++ Take/Custom Cells/RouteCell.swift
@@ -17,4 +17,43 @@ class RouteCell: UITableViewCell {
     @IBOutlet private weak var areaButton: UIButton!
     @IBOutlet private weak var difficultyLabel: UILabel!
     @IBOutlet private weak var typesLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.nameLabel.textColor = .white
+        self.difficultyLabel.textColor = .white
+        self.typesLabel.textColor = .white
+        self.theImageView.roundImage(portion: 2)
+        self.theImageView.addBorder(color: .white, width: 1)
+        self.locationImageView.roundImage(portion: 2)
+        self.locationImageView.addBorder(color: .white, width: 1)
+        // background
+        self.backgroundColor = UIColor.clear
+        self.bgView.backgroundColor = UIColor.black
+        self.bgView.layer.cornerRadius = 10
+        self.bgView.clipsToBounds = true
+        self.selectionStyle = .none
+        self.backgroundColor = .clear
+    }
+
+    func setAreaAbrev(with newText: String) {
+        self.areaButton.addAbrevText(text: newText)
+    }
+    func setAreaButtonTitle() {
+        self.areaButton.setTitle("", for: .normal)
+    }
+
+    func setImage(with newImage: UIImage) {
+        self.theImageView.image = newImage
+    }
+    func setLocationImage(with newImage: UIImage) {
+        self.locationImageView.image = newImage
+    }
+
+    func setLabels(name: String, types: String, difficulty: String) {
+        self.nameLabel.text = name
+        self.typesLabel.text = types
+        self.difficultyLabel.text = difficulty
+    }
+
 }
