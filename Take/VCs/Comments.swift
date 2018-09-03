ac71502fa2e49f442a89ea85ae1467520f791366
nathanmacfarlane <nathanmmacfarlane@gmail.com>
2018-09-02 23:10:26 -0700

    fully functioning comments... so stoked

--- Take/VCs/Comments.swift
+++ Take/VCs/Comments.swift
@@ -6,14 +6,15 @@
 //  Copyright © 2018 N8. All rights reserved.
 //
 
+import CodableFirebase
+import FirebaseAuth
+import FirebaseFirestore
 import UIKit
 
-class Comments: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class Comments: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextViewDelegate {
 
     // MARK: - IBOulets
-    @IBOutlet private weak var searchBG: UIView!
     @IBOutlet private weak var myTableView: UITableView!
-    @IBOutlet private weak var freqWordsLabel: UILabel!
     @IBOutlet private weak var userCommentView: UIView!
     @IBOutlet private weak var userCommentNameLabel: UILabel!
     @IBOutlet private weak var userCommentImageView: UIImageView!
@@ -24,16 +25,41 @@ class Comments: UIViewController, UITableViewDelegate, UITableViewDataSource {
     // MARK: - Variables
     var theRoute: Route!
     let dateFormatterPrint: DateFormatter = DateFormatter()
-    var id: String = "IDdbKJxtW9gGxaxHncMaJzTIb9j2"
+    let forFsSave: DateFormatter = DateFormatter()
+    var userId: String = ""
+    var comments: [Comment] = []
 
     // MARK: - View load/unlaod
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        dateFormatterPrint.dateFormat = "M.dd.yy"
-        let topWords = self.mostFreqWords()
-        self.searchBG.roundView(portion: 10)
-        self.freqWordsLabel.text = topWords.joined(separator: ", ")
+        userCommentField.text = "Placeholder"
+        userCommentField.textColor = UIColor.lightGray
+        userCommentField.becomeFirstResponder()
+        userCommentField.selectedTextRange = userCommentField.textRange(from: userCommentField.beginningOfDocument, to: userCommentField.beginningOfDocument)
+        userCommentField.resignFirstResponder()
+
+        theRoute.getComments { comments in
+            self.comments = comments.sorted { $0.date > $1.date }
+            self.myTableView.reloadData()
+        }
+
+        if let userId = Auth.auth().currentUser?.uid {
+            self.userId = userId
+            Firestore.firestore().getUser(id: userId) { user in
+                DispatchQueue.main.async {
+                    self.userCommentNameLabel.text = user.name
+                }
+                user.getProfilePhoto { image in
+                    DispatchQueue.main.async {
+                        self.userCommentImageView.image = image
+                    }
+                }
+            }
+        }
+
+        forFsSave.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatterPrint.dateFormat = "M/dd/yy"
         self.userCommentDatelabel.text = dateFormatterPrint.string(from: Date())
         self.userCommentImageView.roundImage(portion: 2)
         self.userCommentImageView.addBorder(color: .white, width: 2)
@@ -43,44 +69,77 @@ class Comments: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     // MARK: - TableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return theRoute.comments.count
+        return self.comments.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? CommentCell else { return UITableViewCell() }
-
-        let topWords = mostFreqWords()
-        let comment = self.theRoute.comments[indexPath.row]
-        for topW in topWords {
-            if comment.text.contains(topW) {
-                //print("cell at index: \(indexPath.row) contains: \(topW)")
+        cell.backgroundColor = UIColor.clear
+        cell.setNameLabel(with: "")
+        let db = Firestore.firestore()
+        db.getUser(id: self.comments[indexPath.row].userId) { user in
+            cell.setNameLabel(with: user.name)
+            user.getProfilePhoto { profileImage in
+                DispatchQueue.main.async {
+                    cell.setUserImage(with: profileImage)
+                }
             }
         }
-
-        cell.backgroundColor = UIColor.clear
-        cell.setNameLabel(with: theRoute.comments[indexPath.row].id)
-        cell.setCommentLabel(with: theRoute.comments[indexPath.row].text)
-        cell.setDateLabel(with: dateFormatterPrint.string(from: theRoute.comments[indexPath.row].date))
+        cell.setCommentLabel(with: self.comments[indexPath.row].text)
+        cell.setDateLabel(with: dateFormatterPrint.string(from: self.comments[indexPath.row].date))
         return cell
     }
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        if self.theRoute.comments[indexPath.row].id == self.id {
+        if self.comments[indexPath.row].userId == self.userId {
             return true
         }
         return false
     }
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        self.theRoute.comments.remove(at: indexPath.row)
+        comments[indexPath.row].delete(route: theRoute)
+        comments.remove(at: indexPath.row)
         self.myTableView.deleteRows(at: [indexPath], with: .fade)
     }
 
     // MARK: - IBActions
     @IBAction private func goPostComment(_ sender: Any) {
         guard let commentText = self.userCommentField.text else { return }
-        let newComment = Comment(id: self.id, text: commentText, date: Date())
+        let commentId = UUID().uuidString
+        let newComment = Comment(id: commentId, userId: self.userId, text: commentText, dateString: forFsSave.string(from: Date()))
         self.userCommentField.resignFirstResponder()
         self.userCommentField.text = ""
-        self.theRoute.comments.insert(newComment, at: 0)
+        self.comments.insert(newComment, at: 0)
         self.myTableView.reloadData()
+        //save to db
+        self.theRoute.commentIds.append(commentId)
+        guard let data = try! FirebaseEncoder().encode(newComment) as? [String: Any] else { return }
+        DispatchQueue.global(qos: .background).async {
+            Firestore.firestore().collection("comments").document(commentId).setData(data)
+            self.theRoute.fsSave()
+        }
+    }
+
+    // MARK: - UITextView
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let currentText: String = textView.text
+        let updatedText = (currentText as NSString).replacingCharacters(in: range, with: text)
+        if updatedText.isEmpty {
+            textView.text = "Placeholder"
+            textView.textColor = UIColor.lightGray
+            textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+        } else if textView.textColor == UIColor.lightGray && !text.isEmpty {
+            textView.textColor = UIColor.black
+            textView.text = text
+        } else {
+            return true
+        }
+        return false
+    }
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        if self.view.window != nil {
+            if textView.textColor == UIColor.lightGray {
+                textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+            }
+        }
     }
 
     // MARK: - Navigation
@@ -95,38 +154,6 @@ class Comments: UIViewController, UITableViewDelegate, UITableViewDataSource {
         theWindow.layer.add(transition, forKey: nil)
         self.dismiss(animated: false, completion: nil)
 
-    }
-
-    // MARK: - other functions
-    func mostFreqWords() -> [String] {
-        var topWords: [String] = []
-        var allWords: String = ""
-        for comment in self.theRoute.comments {
-            allWords.append(" \(comment.text)")
-        }
-
-        let wordsArr = allWords.components(separatedBy: " ")
-        var counts: [String: Int] = [:]
-
-        for item in wordsArr where item.count >= 5 {
-            counts[item] = (counts[item] ?? 0) + 1
-        }
-
-        for _ in 1...3 {
-            var maxKey = ""
-            var maxCount = 0
-            for (key, value) in counts where value > maxCount {
-                maxKey = key
-                maxCount = value
-            }
-
-            if !maxKey.isEmpty {
-                topWords.append(maxKey)
-                counts.removeValue(forKey: maxKey)
-            }
-        }
-
-        return topWords
     }
 
 }
