55ac7c1804d7fee8d8a0ccc3b07550826591a948
nathanmacfarlane <nathanmmacfarlane@gmail.com>
2018-09-10 15:10:55 -0700

    basic swiftydraw

--- Take/VCs/EditARPhoto.swift
+++ Take/VCs/EditARPhoto.swift
@@ -6,68 +6,33 @@
 //  Copyright © 2018 N8. All rights reserved.
 //
 
+import SwiftyDraw
 import UIKit
 
 class EditARPhoto: UIViewController {
 
     // MARK: - IBOutlets
     @IBOutlet private weak var bgImageView: UIImageView!
-    @IBOutlet private weak var myImageView: UIImageView!
     @IBOutlet private weak var backButton: UIButton!
-    @IBOutlet private var panGesture: UIPanGestureRecognizer!
-    @IBOutlet private weak var canvasView: UIView!
+    @IBOutlet private weak var canvasView: SwiftyDrawView!
 
     // MARK: - variables
     var theRoute: Route!
-    var theImage: UIImage = UIImage()
-    var path: UIBezierPath = UIBezierPath()
-    var startPoint: CGPoint = CGPoint()
-    var touchPoint: CGPoint = CGPoint()
+    var theImage: UIImage!
+    var path: UIBezierPath!
+    var startPoint: CGPoint!
+    var touchPoint: CGPoint!
     let paintColor: UIColor = UIColor(hexString: "A6D7FF")
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        canvasView.clipsToBounds = true
-        canvasView.isMultipleTouchEnabled = false
+//        let drawView = SwiftyDrawView(frame: self.view.frame)
+//        self.view.addSubview(drawView)
 
-        self.myImageView.image = theImage
         self.bgImageView.image = theImage
         self.backButton.roundButton(portion: 4)
 
-    }
-
-    // MARK: - Touches
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first
-        if let point = touch?.location(in: self.canvasView) {
-            startPoint = point
-        }
-    }
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first
-        if let point = touch?.location(in: self.canvasView) {
-            touchPoint = point
-        }
-
-        path.move(to: startPoint)
-        path.addLine(to: touchPoint)
-        startPoint = touchPoint
-        draw()
-    }
-    func draw() {
-        let strokeLayer = CAShapeLayer()
-        strokeLayer.fillColor = nil
-        strokeLayer.lineWidth = 5
-        strokeLayer.strokeColor = paintColor.cgColor
-        strokeLayer.path = path.cgPath
-        self.canvasView.layer.addSublayer(strokeLayer)
-        self.canvasView.setNeedsDisplay()
-    }
-    func removeLines() {
-        path.removeAllPoints()
-        canvasView.layer.sublayers = nil
-        canvasView.setNeedsDisplay()
     }
 
     // MARK: - Navigation
@@ -81,7 +46,6 @@ class EditARPhoto: UIViewController {
             presenter.arKeys.append(imageId)
             presenter.selectedAr.updateValue([theImage, self.canvasView.asImage()], forKey: imageId)
         }
-        self.removeLines()
         self.dismiss(animated: true, completion: nil)
     }
 
